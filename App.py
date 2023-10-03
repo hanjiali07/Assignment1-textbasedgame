968c53579f1eb3bf1e3283250784db97f3d81bc8
Hanjia <hanjia.li02@gmail.com>
2023-10-02 23:16:32 -0400

    added intro and other required files

---
+++ App.py
@@ -0,0 +1,14 @@
+print("In the quiet town of Stars Hallow, the Edencrest Manor, a magnificent mansion, held dark secrets. One fateful night, a chilling scream shattered the peace as Lord Christian Dior was found murdered in his study.")
+print("---")
+print("The Stars Hollow Constabulary, led by Inspector Montgomery, was summoned to investigate the crime. Suspicion hung heavy as they interrogated staff and scoured the mansion for clues.")
+print("---")
+print("Meanwhile, in the shadows of the mansion, a figure lurked—the Murderer. A master of disguise and deceit, the Murderer had been living in the shadows of the manor, observing the unfolding events with bated breath. Their aim was to eliminate any trace that could lead to their apprehension and make a daring escape from the mansion unnoticed.")
+print("---")
+print("You stand at the mansion's threshold, facing a choice: Will you embrace the darkness as the Murderer, seeking to outwit the Inspector and escape the clutches of justice, or will you step into the shoes of the Detective, determined to unravel the secrets of Eboncrest Manor and bring the murderer to justice? The choice is yours, and the fate of stars hallow hangs in the balance.")
+print("---")
+
+storyDecision = input("Do you want to be the murderer or the detective?: ")
+if storyDecision == "murderer":
+    print("I'm the murderer")
+else:
+    print("I'm the detective")
